dacb550048101eedfb788c492f4b90fe7f2f1251
Sofi.Maye <sofi.mann99@gmail.com>
2022-03-14 14:23:54 +0200

    created new product cards, added new images

--- js/home.js
+++ js/home.js
@@ -1,73 +1,196 @@
-const createCard = function ({discount, image, brand, shortDescription, actualPrice, previousPrice}) {
-    let prodContainer = document.querySelector('.product-container');
+function createCategorySection(categoryName, products) {
+    // add new section
+    // for each product in products add to newly created section
+    let mainSectionContainer = document.querySelector(".main-products-container");
 
-    let prodCard = document.createElement('div')
+    let newSection = document.createElement("section");
+    newSection.className = "product";
+
+    mainSectionContainer.appendChild(newSection);
+
+    let prodCategory = document.createElement('h2');
+    prodCategory.className = "product-category";
+    prodCategory.innerHTML = `${categoryName}`;
+
+    newSection.appendChild(prodCategory);
+
+    let prevBtn = document.createElement("button");
+    prevBtn.className = "pre-btn";
+
+    let buttonPrevImg = document.createElement("img");
+    buttonPrevImg.src = "images/left%20arrow.png";
+    buttonPrevImg.alt = "";
+
+    prevBtn.appendChild(buttonPrevImg);
+    newSection.appendChild(prevBtn);
+
+    let nextBtn = document.createElement("button");
+    nextBtn.className = "nxt-btn";
+
+    let buttonNextImg = document.createElement("button");
+    buttonNextImg.src = "images/right%20arrow.png";
+    buttonNextImg.alt = "";
+
+    nextBtn.appendChild(buttonNextImg);
+    newSection.appendChild(nextBtn);
+
+    let newProdCard = document.createElement("div");
+    newProdCard.className = "product-container";
+    newProdCard.id = `${categoryName}`;
+
+    newSection.appendChild(newProdCard);
+
+
+    for (let prod of products) {
+        createCard(categoryName, prod)
+    }
+}
+
+
+const createCard = function (containerId, {
+    discount, image, brand, shortDescription, actualPrice,
+    previousPrice = "the same price"
+}) {
+    let prodContainer = document.querySelector(`#${containerId}.product-container`);
+
+    let prodCard = document.createElement('div');
     prodCard.className = "product-card";
     prodCard.innerHTML = `
         <div class="product-image">
-            <span class="discount-tag">${discount}</span>
             <img src=${image} class="product-thumb" alt="">
                 <button class="card-btn">add to wishlist</button>
         </div>
         <div class="product-info">
             <h2 class="product-brand">${brand}</h2>
             <p class="product-short-description">${shortDescription}</p>
-            <span class="price">${actualPrice}</span><span class="actual-price">${previousPrice}</span>
+            <span class="price">${actualPrice}</span>
         </div>`
 
-    prodContainer.appendChild(prodCard)
+        if (`${discount}`) {
+            let prodImgCont = document.querySelector(".product-image");
+            let newSaleSpan = document.createElement('span');
+            newSaleSpan.className = "discount-tag";
+            newSaleSpan.innerHTML = `${discount}`;
+
+            prodImgCont.appendChild(newSaleSpan)
+        }
+
+        if (`${previousPrice}`) {
+            let prodInfo = document.querySelector(".product-info");
+            let newInfoSpan = document.createElement('span');
+            newInfoSpan.className = "actual-price";
+            newInfoSpan.innerHTML = `${previousPrice}`;
+
+
+            prodInfo.appendChild(newInfoSpan)
+        }
+
+    prodContainer.appendChild(prodCard);
 
 }
-createCard({
+
+// <span className="actual-price">${previousPrice}</span>
+// <span className="discount-tag">${discount}</span>
+
+
+createCategorySection("sale", [{
     discount: '-50%',
     image: "images/guccimarmount.jpg",
     brand: "gucci",
     shortDescription: "saffiano marmount leather bag",
     actualPrice: "$1000",
     previousPrice: "$2000"
-})
-createCard({
+}, {
     discount: '-50%',
     image: "images/dressfaithfull.jpg",
     brand: "faithfull",
     shortDescription: "short black linen dress",
     actualPrice: "$500",
     previousPrice: "$250"
-})
-createCard({
+}, {
     discount: '-50%',
     image: "images/guccihorsebit1955.jpg",
     brand: "gucci",
     shortDescription: "leather bag",
     actualPrice: "$1000",
     previousPrice: "$2000"
-})
-createCard({
+}, {
     discount: '-50%',
     image: "images/dressselfportrait.jpg",
     brand: "selfportrait",
     shortDescription: "short dress",
     actualPrice: "$300",
     previousPrice: "$600"
-})
-
-createCard({
+}, {
     discount: '-50%',
     image: "images/dressparosh.jpg",
     brand: "parosh",
     shortDescription: "short dress",
     actualPrice: "$300",
     previousPrice: "$600"
-})
-
-createCard({
+}, {
     discount: '-50%',
     image: "images/dressdavidkoma.jpg",
     brand: "david koma",
     shortDescription: "short dress",
     actualPrice: "$400",
     previousPrice: "$800"
-})
+}]);
+
+
+createCategorySection("shoes", [{
+    image: "images/burberry.jpg",
+    brand: "burberry",
+    shortDescription: "brown leather shoes",
+    actualPrice: "$500"
+}, {
+    image: "images/guccishoes.jpg",
+    brand: "gucci",
+    shortDescription: "gucci black leather shoes",
+    actualPrice: "$700"
+}, {
+    image: "images/louboutin.jpg",
+    brand: "louboutin",
+    shortDescription: "louboutin black shoes",
+    actualPrice: "$1000"
+}, {
+    image: "images/burberry2.jpg",
+    brand: "burberry",
+    shortDescription: "lether shoes",
+    actualPrice: "$1000"
+}, {
+    image: "images/guccishoes2.jpg",
+    brand: "gucci",
+    shortDescription: "saffiano marmount leather bag",
+    actualPrice: "$1000"
+}]);
+
+createCategorySection("jackets", [{
+    image: "images/bakhirkajacket.jpg",
+    brand: "bakhirka",
+    shortDescription: "jacket",
+    actualPrice: "$500"
+}, {
+    image: "images/basemntjacket.jpg",
+    brand: "basemnt",
+    shortDescription: "jacket",
+    actualPrice: "$800"
+}, {
+    image: "images/basemntjacket.jpg",
+    brand: "basemnt",
+    shortDescription: "jacket",
+    actualPrice: "$800"
+}, {
+    image: "images/basemntjacket.jpg",
+    brand: "basemnt",
+    shortDescription: "jacket",
+    actualPrice: "$800"
+}, {
+    image: "images/basemntjacket.jpg",
+    brand: "basemnt",
+    shortDescription: "jacket",
+    actualPrice: "$800"
+}])
 
 
 // to think about better implementation
@@ -88,33 +211,3 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
-
-
-//creating collections
-const createCollection = function ({image, category}) {
-    let collectionCont = document.querySelector('.collection-container');
-
-    let collectionCard = document.createElement('a');
-    collectionCard.className = 'collection';
-    collectionCard.href = "#";
-    collectionCard.innerHTML = `<img src=${image} alt="new collection">
-            <p class="collection-title">${category}<br>apparels</p> `
-
-    collectionCont.appendChild(collectionCard)
-}
-
-createCollection({
-    image: 'images/dressandflowers.jpg',
-    category: 'women'
-})
-
-createCollection({
-    image: 'images/menstyle.jpg',
-    category: 'men'
-})
-
-createCollection({
-    image: 'images/bagscollection.jpg',
-    category: 'bags'
-})
-
