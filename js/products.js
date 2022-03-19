c9a559705f5edca8741746040b31579aab17793f
Sofi.Maye <sofi.mann99@gmail.com>
2022-03-19 21:53:28 +0200

    made a search on the product page

--- js/products.js
+++ js/products.js
@@ -11,24 +11,53 @@ import {createCategorySection} from "./ui/products.js";
 
 
 let buttonSearch = document.querySelector('.search-btn');
+
 buttonSearch.addEventListener("click", () => {
+
     let input = document.querySelector(".search-box");
-    let filter = input.value.toUpperCase();
+    let filter = input.value;
 
     // let allProducts = document.querySelectorAll(".product-container");
     searchCategoriesByName({name: filter}).then(categories => {
         //повертаються всі категорії які відповідають пошуку
+
+
+        // clean up search results
+        let searchCont = document.querySelector(".search-results");
+        while (searchCont.firstChild) {
+            searchCont.removeChild(searchCont.firstChild);
+        }
+        //якщо категорії немає у списку
+        if (categories.length === 0) {
+            let searchRes = document.createElement("h2")
+            searchRes.className = "heading";
+
+            searchCont.prepend(searchRes);
+
+            searchRes.innerHTML = `no matches for ${filter}` ;
+
+
+        } else {
+            let searchRes = document.createElement("h2")
+            searchRes.className = "heading";
+
+            searchCont.prepend(searchRes);
+
+            searchRes.innerHTML = `Matches for ${filter}` ;
+            const productsContainer = document.createElement("section");
+            productsContainer.className = "main-products-container";
+            searchCont.appendChild(productsContainer)
+        }
+
         for (let category of categories) {
             getProductsByCategory({categoryId: category.id})
                 .then((products) => createCategorySection(category.name, products))
         }
     })
-
-    //очистить результаты поиска
+    //почистите результати пошуку
     input.value = "";
 
     //при повторном поиске стирать предыдущие данные поиска со страницы
-
-
+   //нам нужно сравнить 2 категории и если совпадают то стереть предыдущую
 })
 
