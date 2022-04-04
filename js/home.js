bc112f15f4a9d295543ceb9595f80aec33b2ffa4
Sofi.Maye <sofi.mann99@gmail.com>
2022-04-04 15:00:08 +0300

    added products with sales to home.html

--- js/home.js
+++ js/home.js
@@ -1,14 +1,8 @@
 import {createCategorySection} from "./ui/products.js";
-import {getCategoryById, getProductsWithDiscount} from "./data.js"
+import {getProductsWithDiscount} from "./data.js"
 
 
-// подумати як зробити sale єдиним
 getProductsWithDiscount().then((productsWithSales) => {
-    const categoryIds = [...new Set(productsWithSales.map((p) => p.categoryId))]
-    Promise.all(categoryIds.map(id => getCategoryById({id: id}))).then(categories => {
-        for (let category of categories){
-            let newProducts = productsWithSales.filter(p => p.categoryId === category.id);
-            createCategorySection(category.name, newProducts);
-        }
-    })
+    createCategorySection("Sale", productsWithSales);
+
 })
