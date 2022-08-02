63f3970b9fd00dfc7404113a1fa66b629c49cd53
Sofi.Maye <sofi.mann99@gmail.com>
2022-08-02 12:34:50 +0300

    added sale.js

---
+++ src/sale.js
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+import {getProductsWithDiscount} from "./data/data";
+import {Product} from "./new";
+
+export default function SalePage(){
+    const [productsWithSales, setProductsWithSales] = useState();
+    useEffect(() => {
+        getProductsWithDiscount().then(setProductsWithSales)
+    })
+    return(
+        <section className="main-sale-container">
+            {productsWithSales ? productsWithSales.map((product) => <Product key={product.id} product={product}/>) : "products downloading"}
+        </section>
+    )
+}
